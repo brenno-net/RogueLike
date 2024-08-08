08bf418420406e0f4ce2114414d134e6ab178d81
Brenno Pereira <brennopereira007@gmail.com>
2024-08-07 23:51:37 -0300

    retorno para a paleta original, voltarei futuramente para testes de novas cores

--- RogueLike/tile_types.py
+++ RogueLike/tile_types.py
@@ -39,12 +39,12 @@ SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 floor = new_tile(
     walkable=True,
     transparent=True,
-    dark=(ord(" "), (255, 255, 255), (155, 188, 15)),
-    light=(ord(" "), (255, 255, 255), (202, 220, 159)),
+    dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    light=(ord(" "), (255, 255, 255), (200, 180, 50)),
 )
 wall = new_tile(
     walkable=False,
     transparent=False,
-    dark=(ord(" "), (255, 255, 255), (15, 56, 15)),
-    light=(ord(" "), (255, 255, 255), (139, 172, 15)),
+    dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    light=(ord(" "), (255, 255, 255), (130, 110, 50)),
 )
