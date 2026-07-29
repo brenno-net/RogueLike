ee00324c1e18308903e88b00c6ba65de86a902dc
Brenno Pereira <brennopereira007@gmail.com>
2026-07-29 11:16:03 -0300

    commit

--- RogueLike/main.py
+++ RogueLike/main.py
@@ -3,6 +3,7 @@ import copy
 
 import tcod
 
+import color
 from engine import Engine
 import entity_factories
 from procgen import generate_dungeon
@@ -13,7 +14,7 @@ def main() -> None:
     screen_height = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
@@ -41,6 +42,8 @@ def main() -> None:
     )
 
     engine.update_fov()
+    
+    engine.me
 
     with tcod.context.new_terminal(
         screen_width,
